634a6fb2d4aa4cec41b41426dcf06531d8273100
Unknown <alexandar.klissurov@gmail.com>
2017-12-06 17:07:12 +0100

    Some sounds.

---
+++ IndieProject/Assets/PlayerMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMusic : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
